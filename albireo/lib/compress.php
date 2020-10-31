dc3fa1c3ba88fd8ad1ac6b440c515e484e28db55
MAX <maxsite.org@gmail.com>
2020-10-31 16:44:05 +0200

    Правки по сжатию html-кода

--- albireo/lib/compress.php
+++ albireo/lib/compress.php
@@ -1,7 +1,6 @@
 <?php if (!defined('BASE_DIR')) exit('No direct script access allowed');
 /**
  * (c) MaxSite CMS, https://max-3000.com/
- * 
  */
 
 /**
@@ -12,11 +11,11 @@
  */
 function compress_html($text)
 {
-	// защищенный текст
-	$text = preg_replace_callback('!(<pre.*?>)(.*?)(</pre>)!is', '_mso_protect_pre', $text);
-	$text = preg_replace_callback('!(<code.*?>)(.*?)(</code>)!is', '_mso_protect_pre', $text);
-	$text = preg_replace_callback('!(<script.*?>)(.*?)(</script>)!is', '_mso_protect_script', $text);
-	$text = preg_replace_callback('!(<style.*?>)(.*?)(</style>)!is', '_mso_protect_script', $text);
+	// защищенный текст, который не нужно сжимать
+	$text = preg_replace_callback('!(<pre.*?>)(.*?)(</pre>)!is', '_compress_html_protect', $text);
+	$text = preg_replace_callback('!(<code.*?>)(.*?)(</code>)!is', '_compress_html_protect', $text);
+	$text = preg_replace_callback('!(<script.*?>)(.*?)(</script>)!is', '_compress_html_protect', $text);
+	$text = preg_replace_callback('!(<style.*?>)(.*?)(</style>)!is', '_compress_html_protect', $text);
 
 	// сжатие
 	$text = str_replace("\r", "", $text);
@@ -42,49 +41,15 @@ function compress_html($text)
 }
 
 /**
- *  pre, которое загоняется в [html_base64]
- *  callback
- * 
- *  @param $matches matches
- *  @return string
- */
-function _mso_protect_pre($matches)
-{
-	$text = trim($matches[2]);
-
-	$text = str_replace('<p>', '', $text);
-	$text = str_replace('</p>', '', $text);
-	$text = str_replace('[', '&#91;', $text);
-	$text = str_replace(']', '&#93;', $text);
-	$text = str_replace("<br>", "\n", $text);
-	$text = str_replace("<br />", "<br>", $text);
-	$text = str_replace("<br/>", "<br>", $text);
-	$text = str_replace("<br>", "\n", $text);
-
-	$text = str_replace('<', '&lt;', $text);
-	$text = str_replace('>', '&gt;', $text);
-	$text = str_replace('&lt;pre', '<pre', $text);
-	$text = str_replace('&lt;/pre', '</pre', $text);
-	$text = str_replace('pre&gt;', 'pre>', $text);
-
-	$text = $matches[1] . "\n" . '[html_base64]' . base64_encode($text) . '[/html_base64]' . $matches[3];
-
-	return $text;
-}
-
-/**
  *  script и style, которые загоняются в [html_base64]
- *  callback
+ *  callback-функция
  * 
  *  @param $matches matches
  *  @return string
  */
-function _mso_protect_script($matches)
+function _compress_html_protect($m)
 {
-	$text = trim($matches[2]);
-	$text = $matches[1] . '[html_base64]' . base64_encode($text) . '[/html_base64]' . $matches[3];
-
-	return $text;
+	return $m[1] . '[html_base64]' . base64_encode($m[2]) . '[/html_base64]' . $m[3];
 }
 
 # end of file
