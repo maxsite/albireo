730b42c85834dfbda0895c757b58d93f9f62e43f
MAX <maxsite.org@gmail.com>
2020-11-22 19:21:35 +0200

    Ctrl+S в редакторе и индикатор изменений

--- albireo-data/pages/admin/modules/edit/_edit-editor.php
+++ albireo-data/pages/admin/modules/edit/_edit-editor.php
@@ -12,13 +12,13 @@ $urlRedirect =  SITE_URL . 'admin/pages';
 ?>
 <div class="h100vh-min flex flex-column">
     <div class="flex-grow0 flex flex-vcenter pad20-tb bg-yellow250 pad30-rl">
-        <h1 class="flex-grow5 h3 mar0"><?= str_replace('\\', '/', $fileEdit) ?></h1>
+        <h1 class="flex-grow5 h3 mar0"><?= str_replace('\\', '/', $fileEdit) ?><sup id="flagModified" class="t-gray600"></sup></h1>
         <div class="im-times cursor-pointer hover-t-red600" onclick="deleteFile('<?= $segmentFile ?>')">Delete file</div>
     </div>
 
-    <form class="pad20-tb pad30-rl flex-grow5 flex flex-column" onsubmit="sendAjax(this); return false;">
+    <form id="formEdit" class="pad20-tb pad30-rl flex-grow5 flex flex-column" onsubmit="sendAjax(this); return false;">
         <div class="flex-grow5 h100px bg-blue">
-            <textarea class="w100 h100 t-mono lh130" style="tab-size: 4;" name="content" id="content"><?= $content ?></textarea>
+            <textarea class="w100 h100 t-mono lh130" style="tab-size: 4;" name="content" id="content" autofocus><?= $content ?></textarea>
             <input type="hidden" name="_method" value="AJAX">
             <input type="hidden" name="file" value="<?= $segmentFile ?>">
         </div>
@@ -28,7 +28,7 @@ $urlRedirect =  SITE_URL . 'admin/pages';
                 <button class="button button1 im-check pad20-rl" type="submit">Save</button>
             </div>
 
-            <div class="flex-grow5 t-red600 pad20-rl" id="result"></div>
+            <div class="flex-grow5 t-red600 pad20-rl" id="result"><span class="t-gray600">Press <kbd>Ctrl+S</kbd> for save</span></div>
         </div>
     </form>
 </div>
@@ -42,6 +42,7 @@ $urlRedirect =  SITE_URL . 'admin/pages';
             if (this.readyState == 4 && this.status == 200) {
                 document.getElementById("result").innerHTML =
                     this.responseText;
+                document.getElementById("flagModified").innerHTML = '';
             }
         };
 
@@ -63,12 +64,28 @@ $urlRedirect =  SITE_URL . 'admin/pages';
                     document.getElementById("result").innerHTML =
                         this.responseText;
                     document.location.href = "<?= $urlRedirect ?>";
-            }
-        };
+                }
+            };
 
-        xhttp.open("POST", "<?= $urlAjaxDelete ?>", true);
-        xhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-        xhttp.send(form);
+            xhttp.open("POST", "<?= $urlAjaxDelete ?>", true);
+            xhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+            xhttp.send(form);
+        }
     }
-    }
+
+    document.getElementById("content").addEventListener('input',
+        function(e) {
+            document.getElementById("flagModified").innerHTML = ' changed';
+        }
+    );
+
+    document.getElementById("content").addEventListener('keydown',
+        function(e) {
+            if (e.ctrlKey && e.code == 'KeyS') {
+                sendAjax(document.getElementById("formEdit"));
+                e.preventDefault();
+            }
+        }
+    );
+
 </script>
